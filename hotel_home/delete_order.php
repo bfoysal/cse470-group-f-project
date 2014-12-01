35a0da7802ffe333474fbd87c28ca3e2a57a493d
bfoysal <burhan420@gmail.com>
2014-12-01 22:36:45 +0600

    delete order query changed
    query has been updated so the room/ row will not get deleted upon
    deleting the order.

--- hotel_home/delete_order.php
+++ hotel_home/delete_order.php
@@ -3,7 +3,7 @@ session_start();
 $room_no=$_GET['room_no'];
 $cust_id=$_SESSION['session_cust_id'];
 require_once 'config.php';
-$sql = "DELETE FROM rooms WHERE room_no='$room_no' AND cust_id='$cust_id'";
+$sql = "UPDATE rooms SET status='vacant',check_in='',check_out='',cust_id=NULL WHERE room_no='$room_no' AND cust_id='$cust_id'";
 $result=mysql_query($sql);
 if($result === FALSE) {
 	die(mysql_error());
